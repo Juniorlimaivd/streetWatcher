8ca2bf2e55decc18da67a9efb4f566751f432f6d
Júnior Lima <jnolj@bepid.cin.ufpe.br>
2017-01-28 17:34:29 -0300

    correcting service account name

--- firebaseManager.py
+++ firebaseManager.py
@@ -12,7 +12,7 @@ class FirebaseManager:
 			"authDomain" : "calcada-34af9.firebaseapp.com",
 			"databaseURL": "https://calcada-34af9.firebaseio.com",
 			"storageBucket" : "calcada-34af9.appspot.com",
-			"serviceAccount" : "calcada-34af9-firebase-adminsdk-44spg-2d4f905b92",
+			"serviceAccount" : "calcada-34af9-firebase-adminsdk-44spg-2d4f905b92.json",
 			"messagingSenderId": "1041897087083"
 		}
 
