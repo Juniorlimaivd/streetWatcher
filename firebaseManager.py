a654785ba5ab35eb2f569a169ebcd280116fcf12
Júnior Lima <jnolj@bepid.cin.ufpe.br>
2017-01-28 17:32:55 -0300

    config firebase manager

--- firebaseManager.py
+++ firebaseManager.py
@@ -5,23 +5,30 @@ class FirebaseManager:
 	firebase = None
 	database = None
 	storage = None
-
+	self.config = None
 	def __init__ (self):
-		config = {
-			"apiKey" : "apiKey",
-			"authDomain" : "projectId.firebaseapp.com",
-			"databaseURL": "https://databaseName.firebaseio.com",
-			"storageBucket" : "projectId.appspot.com"
+		self.config = {
+			"apiKey" : "AIzaSyCdqFd6BOzCnx55lBezdzRq70kfmq8z5eY",
+			"authDomain" : "calcada-34af9.firebaseapp.com",
+			"databaseURL": "https://calcada-34af9.firebaseio.com",
+			"storageBucket" : "calcada-34af9.appspot.com",
+			"serviceAccount" : "calcada-34af9-firebase-adminsdk-44spg-2d4f905b92",
+			"messagingSenderId": "1041897087083"
 		}
 
-		self.firebase = pyrebase.initialize_app(config)
+		self.firebase = pyrebase.initialize_app(self.config)
 
 		self.database = self.firebase.database()
 
 		self.storage = self.firebase.storage()
+
+	def putTeste(self):
+		data = {"name": "Mortimer 'Morty' Smith"}
+		self.database.child("users").push(data)
 
 
 
 
 
 serverManager = FirebaseManager()
+serverManager.putTeste()
